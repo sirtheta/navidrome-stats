5eae87bb5461b21ae92072a991df02248dcd4803
Amir Aref <amiraref808@gmail.com>
2026-06-13 13:52:19 +0330

    rename backend to src

--- src/app/config.py
+++ src/app/config.py
@@ -2,5 +2,3 @@ import os
 
 DB_PATH = os.environ.get("NAVIDROME_DB", "../data/navidrome.db")
 USE_SAMPLE_DATA = not os.path.exists(DB_PATH)
-PORT = int(os.environ.get("PORT", 5000))
-DEBUG = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
